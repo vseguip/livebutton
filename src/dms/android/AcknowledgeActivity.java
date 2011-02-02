7b156b21ba88c96b33517f70219c1b87a830501e
Vicent Seguí <vseguip@gmail.com>
2011-02-02 20:43:33 +0100

    Change acknowledge activity to use the new SMSService
    Signed-off-by: Vicent Seguí <vseguip@gmail.com>

--- src/dms/android/AcknowledgeActivity.java
+++ src/dms/android/AcknowledgeActivity.java
@@ -1,8 +1,7 @@
 package dms.android;
 
-import java.util.Calendar;
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -10,24 +9,26 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class AcknowledgeActivity extends Activity implements Runnable{
+public class AcknowledgeActivity extends Activity implements Runnable {
 	final static int CALL_REQUEST = 1003;
 	final Handler handler = new Handler();
-	int countdown=10;
-	String phoneNumber=""; 
+	int countdown = 10;
+	String phoneNumber = "";
+	String sms = "";
 	TextView textCounter;
-	@Override	
+
+	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acknowledge);
-		phoneNumber = getIntent().getStringExtra("phoneNumber");
+		phoneNumber = getIntent().getStringExtra(LiveButton.PREF_PHONE_NUMBER);
+		sms = getIntent().getStringExtra(LiveButton.PREF_SMS_CONTENT);
 		textCounter = (TextView) findViewById(R.id.timeRemaining);
 		textCounter.setText(Integer.toString(countdown));
 		Utils.playAlarm(this);
-		handler.postDelayed(this,1000);
-		Button buttonAck = (Button)findViewById(R.id.buttonCancelCall);
+		handler.postDelayed(this, 1000);
+		Button buttonAck = (Button) findViewById(R.id.buttonCancelCall);
 		buttonAck.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 				handler.removeCallbacks(AcknowledgeActivity.this);
@@ -36,32 +37,26 @@ public class AcknowledgeActivity extends Activity implements Runnable{
 			}
 		});
 	}
-//	@Override	
-//	public void onView(){
-//		
-//	}
-//	
+
 	@Override
 	public void run() {
-		countdown--;		
+		countdown--;
 		textCounter.setText(Integer.toString(countdown));
-		if(countdown == 0) {
-			//TODO: call number
+		if (countdown == 0) {			
 			countdown = 10;
-			Utils.stopAlarm();			
-			Utils.sendSMS(this, phoneNumber, "User stopped responding at "+ Calendar.getInstance().getTime().toLocaleString());
+			Utils.stopAlarm();
+			Intent newIntent = new Intent(this, SendMessageService.class);
+			newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			newIntent.putExtra("phoneNumber", phoneNumber);
+			newIntent.putExtra("SMS", sms);
+			this.startService(newIntent);
 			finish();
-//			Intent sendIntent = new Intent(Intent.ACTION_VIEW);
-//			sendIntent.putExtra("sms_body", "User failed to acknowledge awareness. "); 
-//			sendIntent.setType("vnd.android-dir/mms-sms");
-//			startActivity(sendIntent);   
 			
-			//startActivityForResult(new Intent(Intent.ACTION_CALL, Uri.parse("tel:+"+phoneNumber)), CALL_REQUEST);
+			
+		} else {
+			handler.postDelayed(this, 1000);
 		}
-		else{
-			handler.postDelayed(this,1000);			
-		}
-		
+
 	};
-	
+
 }
