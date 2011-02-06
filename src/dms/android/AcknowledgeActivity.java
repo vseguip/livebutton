4639c4bce0ed8ff4ec3707bd67000b78bb778519
Vicent Seguí <vseguip@gmail.com>
2011-02-06 11:36:19 +0100

    - Use built in CountDownTimer class
    Signed-off-by: Vicent Seguí <vseguip@gmail.com>

--- src/dms/android/AcknowledgeActivity.java
+++ src/dms/android/AcknowledgeActivity.java
@@ -3,59 +3,53 @@ package dms.android;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class AcknowledgeActivity extends Activity implements Runnable {
+public class AcknowledgeActivity extends Activity {
 	final static int CALL_REQUEST = 1003;
-	final Handler handler = new Handler();
-	int countdown = 10;
-	String phoneNumber = "";
-	String sms = "";
-	TextView textCounter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.acknowledge);
-		phoneNumber = getIntent().getStringExtra(LiveButton.PREF_PHONE_NUMBER);
-		sms = getIntent().getStringExtra(LiveButton.PREF_SMS_CONTENT);
-		textCounter = (TextView) findViewById(R.id.timeRemaining);
-		textCounter.setText(Integer.toString(countdown));
+		final String phoneNumber = getIntent().getStringExtra(LiveButton.PREF_PHONE_NUMBER);
+		final String sms = getIntent().getStringExtra(LiveButton.PREF_SMS_CONTENT);
+		final TextView textCounter = (TextView) findViewById(R.id.timeRemaining);
+		textCounter.setText("10");
 		Utils.playAlarm(this);
-		handler.postDelayed(this, 1000);
+		final CountDownTimer timer = new CountDownTimer(10000, 1000) {
+			@Override
+			public void onTick(long millisUntilFinished) {
+				textCounter.setText(Long.toString(millisUntilFinished/1000));
+			}
+
+			@Override
+			public void onFinish() {
+				Utils.stopAlarm();
+				Intent newIntent = new Intent(AcknowledgeActivity.this, SendMessageService.class);
+				newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				newIntent.putExtra("phoneNumber", phoneNumber);
+				newIntent.putExtra("SMS", sms);
+				AcknowledgeActivity.this.startService(newIntent);
+				finish();
+			}
+		};
+
 		Button buttonAck = (Button) findViewById(R.id.buttonCancelCall);
 		buttonAck.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				handler.removeCallbacks(AcknowledgeActivity.this);
+				timer.cancel();
 				Utils.stopAlarm();
 				AcknowledgeActivity.this.finish();
 			}
 		});
+		timer.start();
 	}
-
-	@Override
-	public void run() {
-		countdown--;
-		textCounter.setText(Integer.toString(countdown));
-		if (countdown == 0) {
-			countdown = 10;
-			Utils.stopAlarm();
-			Intent newIntent = new Intent(this, SendMessageService.class);
-			newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-			newIntent.putExtra("phoneNumber", phoneNumber);
-			newIntent.putExtra("SMS", sms);
-			this.startService(newIntent);
-			finish();
-
-		} else {
-			handler.postDelayed(this, 1000);
-		}
-
-	};
 
 }
