8a59afdc6f490fceef3dafe3fc0db4d2ad07f630
Vicent Seguí <vseguip@gmail.com>
2011-02-08 20:12:42 +0100

    Typo and minior commits
    Signed-off-by: Vicent Seguí <vseguip@gmail.com>

--- src/com/livebutton/SettingsActivity.java
+++ src/com/livebutton/SettingsActivity.java
@@ -4,19 +4,15 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 
-
 public class SettingsActivity extends PreferenceActivity {
-		
-		private static final String LOG_TAG = "SettingsActivity";
 
-		@Override
-	   protected void onCreate(Bundle savedInstanceState) {
-           super.onCreate(savedInstanceState);
-           Log.i(LOG_TAG, "Creating SettingsActivity");
-           addPreferencesFromResource(R.xml.prefs);
-           // Get the custom preference
-	   }
+	private static final String LOG_TAG = "SettingsActivity";
 
-
-		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.i(LOG_TAG, "Creating SettingsActivity");
+		addPreferencesFromResource(R.xml.prefs);
+		// Get the custom preference
+	}
 }
