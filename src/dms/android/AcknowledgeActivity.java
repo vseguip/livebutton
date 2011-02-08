1d9f5cb2479d85260edad338d78f6acf4bd1b268
Vicent Seguí <vseguip@gmail.com>
2011-02-08 19:32:25 +0100

    - Get ringtone preference and use it - Add log messages
    Signed-off-by: Vicent Seguí <vseguip@gmail.com>

--- src/dms/android/AcknowledgeActivity.java
+++ src/dms/android/AcknowledgeActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -12,19 +13,23 @@ import android.widget.TextView;
 
 public class AcknowledgeActivity extends Activity {
 	final static int CALL_REQUEST = 1003;
+	private String LOG_TAG="AcknowledgeActivity";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.i(LOG_TAG, "Creating acknowledge activity");
 		setContentView(R.layout.acknowledge);
+		Log.i(LOG_TAG, "Getting prefs");
 		final String phoneNumber = getIntent().getStringExtra(getString(R.string.phoneNumberPref));
 		final String sms = getIntent().getStringExtra(getString(R.string.SMSContentPref));
 		int countdown = getIntent().getIntExtra(getString(R.string.countDownTimerPref),10);
+		String ringtone = getIntent().getStringExtra(getString(R.string.ringtonePref));
 		if (countdown < 1)
 			countdown = 10;
-		final TextView textCounter = (TextView) findViewById(R.id.timeRemaining);
+		final TextView textCounter = (TextView) findViewById(R.id.timeRemaining);		
 		textCounter.setText(Integer.toString(countdown));
-		Utils.playAlarm(this);
+		Utils.playAlarm(this, ringtone);
 		final CountDownTimer timer = new CountDownTimer(countdown * 1000, 1000) {
 			@Override
 			public void onTick(long millisUntilFinished) {
@@ -33,12 +38,14 @@ public class AcknowledgeActivity extends Activity {
 
 			@Override
 			public void onFinish() {
+				Log.i(LOG_TAG, "Countdown ended without user feedback. Sending message.");
 				Utils.stopAlarm();
 				Intent newIntent = new Intent(AcknowledgeActivity.this, SendMessageService.class);
 				newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 				newIntent.putExtra("phoneNumber", phoneNumber);
 				newIntent.putExtra("SMS", sms);
 				AcknowledgeActivity.this.startService(newIntent);
+				Log.i(LOG_TAG, "Finish activity.");
 				finish();
 			}
 		};
@@ -47,11 +54,14 @@ public class AcknowledgeActivity extends Activity {
 		buttonAck.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				Log.i(LOG_TAG, "Countdown canceled");
 				timer.cancel();
 				Utils.stopAlarm();
+				Log.i(LOG_TAG, "Finishing AcknowledgeActivity");
 				AcknowledgeActivity.this.finish();
 			}
 		});
+		Log.i(LOG_TAG, "Starting countdown");
 		timer.start();
 	}
 
--- src/dms/android/Utils.java
+++ src/dms/android/Utils.java
@@ -12,23 +12,38 @@ import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.widget.Toast;
 
 public class Utils {
+	private static final String LOG_TAG = "Utility";
 	public final static MediaPlayer mediaPlayer = new MediaPlayer();
 
 	static void stopAlarm() {
 		mediaPlayer.stop();
 	}
 
-	static void playAlarm(Activity activity) {
+	static void playAlarm(Activity activity, String ringtone) {
 		try {
 
 			// Uri alert =
 			// RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-			Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-			mediaPlayer.reset();
-			mediaPlayer.setDataSource(activity, alert);
+			Log.i(LOG_TAG, "Playing alarm: " + ringtone);
+			Uri alert;
+			try {
+				if (ringtone == null)
+					alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+				else
+					alert = Uri.parse(ringtone);
+				mediaPlayer.reset();
+				mediaPlayer.setDataSource(activity, alert);
+			} catch (Exception ex) {
+				// some error on the ringtone occurred, try with the defautl one
+				Log.e(LOG_TAG, Log.getStackTraceString(ex));
+				alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+				mediaPlayer.reset();
+				mediaPlayer.setDataSource(activity, alert);
+			}
 			final AudioManager audioManager = (AudioManager) activity.getSystemService(Context.AUDIO_SERVICE);
 			if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
 				mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
@@ -38,6 +53,7 @@ public class Utils {
 			}
 
 		} catch (Exception ex) {
+			Log.e(LOG_TAG, Log.getStackTraceString(ex));
 			Toast.makeText(activity.getBaseContext(), "Could not play alarm", Toast.LENGTH_SHORT).show();
 		}
 	}
